9e1a5d91a4ea70274b82b5098485a2b386af578e
Vaneeecl <jcarreno281@unab.edu.co>
2026-08-20 22:44:25 -0500

    Add files via upload

---
+++ Sistema_Kolb/iniciar_sistema.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d "%~dp0"
+
+echo ==========================================
+echo    SISTEMA DE APOYO - MODELO DE KOLB
+echo ==========================================
+echo.
+echo Iniciando la aplicacion...
+echo.
+
+streamlit run app.py
+
+pause
